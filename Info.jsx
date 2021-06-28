c8e32b11ff57c32b5e6b6c9a6cbad18f97d35450
seniboo <66415641+seniboo@users.noreply.github.com>
2021-06-28 16:47:29 +0530

    Add files via upload

---
+++ Info.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+
+function Info() {
+  return (
+    <div className="note">
+      <h1>Javascript and React.js</h1>
+      <p> A basic web dev React Js Boot camp</p>
+    </div>
+  );
+}
+
+export default Info;
